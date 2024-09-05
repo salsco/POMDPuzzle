c184ffa4abcb2d2d8c6a8181f478543aa02c2cb4
salsco <salvantoscoz@gmail.com>
2024-09-05 15:30:46 +0200

    Minor Bugfixing

--- solver/value_iterator.py
+++ solver/value_iterator.py
@@ -8,7 +8,7 @@ class ValueIterator:
     def value_iteration(self,gamma=1,eps=0.1,max_iter=100):
         dynamics=self.dynamics
         belief_rewards=self.build_immediate_reward(dynamics)
-        obs_given_ba=self.observation_given_belief_action_prob(dynamics)
+        obs_given_ba=self.observation_given_belief_action_prob()
         maximum_belief_rewards=self.maximum_vfunct(belief_rewards)
         max_value_horizon_cur=maximum_belief_rewards
 
@@ -19,9 +19,9 @@ class ValueIterator:
             if(t>=max_iter):
                 break
             print("Iteration number "+str(t+1))
-            transformed_vfunct=self.all_values_transform(dynamics,max_value_horizon_cur)
-            gamma_terms=self.characterize_gamma_terms(dynamics,obs_given_ba)
-            new_horizon=self.finalize_sum(dynamics,gamma_terms,belief_rewards,gamma)
+            transformed_vfunct=self.all_values_transform(max_value_horizon_cur)
+            gamma_terms=self.characterize_gamma_terms(obs_given_ba)
+            new_horizon=self.finalize_sum(gamma_terms,belief_rewards,gamma)
             new_max_value_horizon=self.maximum_vfunct(new_horizon)
             max_value_horizon_cur=new_max_value_horizon
             t+=1
